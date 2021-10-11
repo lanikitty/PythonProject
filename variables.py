8741ee6a3ee701c6038be188e240716ecbfbeeef
TeamM2X <lanianncarag@gmail.com>
2021-10-11 23:35:03 +0800

    data-types

---
+++ variables.py
@@ -0,0 +1,14 @@
+a=20
+b='raj'
+c=True
+d=33.33
+
+print(a,b,c,d)
+
+
+print(type(a), type(b), type(c), type(d))
+
+print(isinstance(a,int))
+print(isinstance(b,int))
+print(isinstance(b,str))
+print(isinstance(c,bool))
