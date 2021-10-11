efa349b0c4ce48b53bf091ab5602751bf4d3b16e
TeamM2X <lanianncarag@gmail.com>
2021-10-11 23:54:16 +0800

    third-commit

---
+++ operators.py
@@ -0,0 +1,26 @@
+#arithmetic operators
+
+print('12 to the power of 3',12**3)
+print('13 divided by 2',13/2)
+print('13 divided by 2 and remainder is ',13%2)
+print('13 floor division 2',13//2)
+
+#relational operator
+print(12>3)
+print(12<3)
+print(12!=12)
+print(12==12)
+print(12<=12)
+print(12>=12)
+
+
+print ('============================================')
+
+#logical operator
+print(12>3 and 12<90)
+print (True and False)
+print(123 and 12)
+print(0 and 123)
+print(True and 142)
+print(123 or 0)
+print(0 or 545)
