993a86633b6ecabaa3f7d7f3cc51cc8985beec34
TeamM2X <lanianncarag@gmail.com>
2021-10-12 09:27:12 +0800

    moretypes

---
+++ moretypes.py
@@ -0,0 +1,22 @@
+one =(1,3,4,3)
+two = [32,534,32,35]
+three=set(one)
+four=set(two)
+five={24,531,154,323,325,123,1235,123,1566,2531,111}
+six={1:2,3:4,5:7}
+
+print(type(one))
+print(type(two))
+print(type(three))
+print(type(four))
+
+print(one)
+print(two)
+print(three)
+print(four)
+print(five)
+
+print(type(six))
+print(six)
+
+
