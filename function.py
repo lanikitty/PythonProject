3f30029396fe422c9d69f0e7f545db12c7e3fc9a
TeamM2X <lanianncarag@gmail.com>
2021-10-12 21:00:30 +0800

    day1

--- function.py
+++ function.py
@@ -1,11 +1,14 @@
 def fun():
     print('fun is running')
 
-def sayHi (a):
-    print("Hi", a)
+def sayHi(a):
+    if isinstance(a,str):
+        print("Hi", a)
+    else:
+        print('Not a right value')
 
 fun()
 
-for x in ['Raj','Ravi','Sam','Lara']:
+print(fun())
+for x in ['Raj','Ravi','Sam','Lara',12,455,True,[1,3254,4]]:
     sayHi(x)
-
