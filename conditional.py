fd07a9169bfd232d25c2a659b96eb17cc3fd337e
TeamM2X <lanianncarag@gmail.com>
2021-10-12 09:03:21 +0800

    conditional

--- conditional.py
+++ conditional.py
@@ -5,5 +5,11 @@ print(len(name))
 if(len(name)>5):
     print('valid name it has more than 4 characters')
     print('we shall store it in the database')
+elif(len(name)==3 or len(name)==4):
+    print('type confirm to store it into database')
+    if 'confirm'==input():
+        print('stored in the database')
+    else:
+        print('wrong input')
 else:
-    print('name should have at least 4 characters')
+    print('name should have at least 3 characters')
